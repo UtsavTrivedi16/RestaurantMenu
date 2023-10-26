511871659e5b30d55bf81652bcb595e31655c61d
utrivedi <utsav.trivedi@aviatnet.com>
2023-10-26 16:39:09 +1300

    Changed app.js to import port and hostname correctly

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const {createServer, startServer} = require('./server');
-const { PORT, HOSTNAME } = require('./config');
+const { port, hostname } = require('./config').serverConfig;
 
 const server = createServer();
-startServer(server, PORT, HOSTNAME);
+startServer(server, port, hostname);
